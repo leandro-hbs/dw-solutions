568c31622a5618177276764f55548796b25ba7dd
leandro-hbs <leandro.batista@academico.ifpb.edu.br>
2019-11-22 11:48:40 -0300

    New Att

---
+++ back-end/php-core/area-circle/leandro.henrique.php
@@ -0,0 +1,8 @@
+<?php
+// A = πr²
+
+function areaOfCircle($radius)
+{
+    $result = 2*$radius*$radius;
+    echo "$result;
+}
---
+++ back-end/php-core/area-circle/leandro.henrique.print.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/../src/area-circle.php';
+
+// Circle Tools
+
+// Area of the circle of radius 1 is of 3.1415926535898
+var_dump(areaOfCircle(1));
+var_dump(3.1415926535898);
+
+// Area of the circle of radius 10 is of 314.15926535898
+var_dump(areaOfCircle(10));
+var_dump(314.15926535898);
