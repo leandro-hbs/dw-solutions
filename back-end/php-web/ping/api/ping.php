047c61892ae96b1b519dc941b0e13045a143a944
leandro-hbs <leandro.batista@academico.ifpb.edu.br>
2019-12-05 16:23:22 -0300

    Atualizaçao

--- back-end/php-web/ping/api/ping.php
+++ back-end/php-web/ping/api/ping.php
@@ -1,8 +1,8 @@
 <?php
-  $count = $_GET['count'] ?? '';
+  $qtd = $_GET['qtd'] ?? '';
   $ip = $_GET['ip'] ?? '';
   $result = [];
-  $result['response'] = shell_exec("ping -c $count $ip");
+  $result['response'] = shell_exec("ping -c $qtd $ip");
   header("Content-type: application/json; charset=UTF-8");
   header("Access-Control-Allow-Origin: *");
   echo json_encode($result);
--- back-end/php-web/ping/public/js/main.js
+++ back-end/php-web/ping/public/js/main.js
@@ -1,11 +1,11 @@
-const count = document.querySelector('#count')
+const qtd = document.querySelector('#qtd')
 const ip = document.querySelector('#ip')
 const submit = document.querySelector('input[type=submit]')
 const ping = document.querySelector('#ping')
 
 submit.addEventListener('click', function(event) {
   event.preventDefault()
-  const url = `../api/ping.php?count=${count.value}&ip=${ip.value}`
+  const url = `../api/ping.php?qtd=${qtd.value}&ip=${ip.value}`
   fetch(url)
     .then(res => res.json())
     .then(json => loadPing(json))
