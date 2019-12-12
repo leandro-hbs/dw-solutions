92681d68678a3103a5e6cdb8b9a53017fc71e4dc
leandro-hbs <leandro.batista@academico.ifpb.edu.br>
2019-12-12 13:34:07 -0300

    Prova DW

--- back-end/php-app/iproute2-app/leandro.batista/api/v1/util.php
+++ back-end/php-app/iproute2-app/leandro.batista/api/v1/util.php
@@ -7,13 +7,13 @@ function interfaces_encode($result)
     preg_match_all($regex, $result, $matches);
     foreach ($matches[1] as $index => $user) {
         $processes[$matches[1][$index]] = [
-            "id"    => $matches[1][$index],
-            "name"     => $matches[2][$index],
-            "mtu"     => $matches[3][$index],
+            "id"        => $matches[1][$index],
+            "name"      => $matches[2][$index],
+            "mtu"       => $matches[3][$index],
             "state"     => $matches[4][$index],
-            "link"     => $matches[5][$index],
-            "mac"     => $matches[6][$index],
-            "macbdr"     => $matches[7][$index],
+            "link"      => $matches[5][$index],
+            "mac"       => $matches[6][$index],
+            "macbdr"    => $matches[7][$index],
         ];
     }
     $regex = "/(\d*).*\s+.*\s+inet (\d*.\d*.\d*.\d*)\/(\d)/";
